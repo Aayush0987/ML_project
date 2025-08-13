b11e86f9623c6db6f66ad65daa57d3de3c891638
Aayush0987 <kumaraayush02@yahoo.com>
2025-08-14 00:41:24 +0530

    EDA & Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail: sys):
     """
@@ -24,7 +25,7 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-'''      
+'''
 if __name__ == "__main__":
     try:
         a = 1 / 0  # This will raise a ZeroDivisionError
