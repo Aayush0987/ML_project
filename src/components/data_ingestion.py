a8ec121a58914934b363de3d41fb1941653ac165
Aayush0987 <kumaraayush02@yahoo.com>
2025-08-14 17:29:09 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,8 +5,14 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+import numpy as np
 
-from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
+
+from src.components.data_transformation import DataTransformationConfig
+
+
 
 @dataclass
 class DataIngestionConfig:
@@ -35,16 +41,30 @@ class DataIngestion:
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
             logging.info("Ingestion of the data is completed")
+
+            preprocessor_path = DataTransformationConfig().preprocessor_obj_file_path
+            logging.info(f"Preprocessor path resolved to: {preprocessor_path}")
+
             return (
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path
+                self.ingestion_config.test_data_path,
+                preprocessor_path
             )
         except Exception as e:
             raise CustomException(e, sys)
         
+
 if __name__ == "__main__":
     obj = DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+    
+    # Unpack three values instead of two
+    train_data, test_data, preprocessor_path = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    
+    # unpack three returned values instead of two
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    # now preprocessor_path is defined here
+    print(modeltrainer.initiate_model_trainer(train_array=train_arr, test_array=test_arr, preprocessor_path=preprocessor_path))
